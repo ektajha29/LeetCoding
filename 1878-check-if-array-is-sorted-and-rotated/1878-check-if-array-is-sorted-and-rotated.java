83080a8eafe266fba6bd383b327558c068f418ad
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-06-16 20:54:11 +0530

    Time: 0 ms (100%), Space: 40.7 MB (95.24%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -15,11 +15,6 @@ class Solution {
 
     public static void main(String[] args) {
         int[] arr1 = {2, 5, 7, 8};
-        int[] arr2 = {7, 8, 2, 5};
-        int[] arr3 = {8, 7, 2, 5};
-
-        System.out.println(check(arr1)); 
-        System.out.println(check(arr2));
-        System.out.println(check(arr3)); 
+        System.out.println(check(arr1));
     }
 }
