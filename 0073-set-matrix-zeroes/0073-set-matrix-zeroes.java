4043f76f607d252580dcc2e19a2c50a6275da214
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-07-16 12:41:46 +0530

    Time: 1 ms (81.08%), Space: 45.6 MB (36.22%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -19,13 +19,9 @@ class Solution {
                 }
             }
         }
- 
-        // Mark with 0 from (1,1) to (n-1, m-1):
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (matrix[i][j] != 0) {
- 
-                    // Check for col & row:
                     if (matrix[i][0] == 0
                         || matrix[0][j] == 0) {
                         matrix[i][j] = 0;
@@ -33,8 +29,6 @@ class Solution {
                 }
             }
         }
- 
-        // Finally mark the 1st row then 1st col:
         if (matrix[0][0] == 0) {
             Arrays.fill(matrix[0], 0);
         }
@@ -44,24 +38,4 @@ class Solution {
             }
         }
     }
- 
-    // Driver Code
-    public static void main(String[] args)
-    {
-        int[][] matrix = { { 0, 1, 2, 0 },
-                           { 3, 4, 5, 2 },
-                           { 1, 3, 1, 5 } };
- 
-        // Function call
-        setZeroes(matrix);
- 
-        // Print the resulting matrix
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 }
-    
