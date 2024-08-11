e4c378a3049f743bc60cda4812ef590a2f89c476
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-11 20:39:48 +0530

    Time: 2 ms (81.89%), Space: 45.3 MB (7.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -10,17 +10,12 @@ class Solution {
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < 0) {
-                // Swap currMax and currMin when a negative number is encountered
                 int temp = currMax;
                 currMax = currMin;
                 currMin = temp;
             }
-
-            // Calculate the maximum and minimum product up to the current number
             currMax = Math.max(nums[i], currMax * nums[i]);
             currMin = Math.min(nums[i], currMin * nums[i]);
-
-            // Update the global maximum product
             maxProduct = Math.max(maxProduct, currMax);
         }
 
