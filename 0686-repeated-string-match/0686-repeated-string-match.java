7c8e771eafab899a88339a1274706d8b002aec75
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-19 00:45:33 +0530

    Time: 259 ms (73.35%), Space: 42 MB (84.05%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,19 +1,22 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-       int repeatCount=1;
-       String repeatedA=a;
-       while(repeatedA.length()<b.length()){
-        repeatedA+=a;
-        repeatCount++;
-       }
-       if(repeatedA.contains(b)){
-        return repeatCount++;
-       }
-       repeatedA+=a;
-       if(repeatedA.contains(b)){
-        return repeatCount+1;
-       }
-    
-    return -1;
-}
+ int repeatCount = 1;
+        StringBuilder repeatedA = new StringBuilder(a);
+
+        while (repeatedA.length() < b.length()) {
+            repeatedA.append(a);
+            repeatCount++;
+        }
+
+        if (repeatedA.toString().contains(b)) {
+            return repeatCount;
+        }
+
+        repeatedA.append(a);
+        if (repeatedA.toString().contains(b)) {
+            return repeatCount + 1;
+        }
+
+        return -1;
+    }
 }
