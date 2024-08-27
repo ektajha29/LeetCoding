14aa549825ff0d71fd2491c24f5c25cabc7b1e5b
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-27 18:35:14 +0530

    Time: 1 ms (99.83%), Space: 46.1 MB (33.84%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -6,9 +6,4 @@ class Solution {
         }
         return xorr;
     }
-    public static void main(String[]args){
-        int arr[]={4,1,2,1,2};
-        int ans=singleNumber(arr);
-        System.out.println("The no that appears only once"+ans);
-    }
 }
