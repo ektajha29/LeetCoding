ab5590ef309e2898a2a2ba9a747af52675e3bf09
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-27 17:18:35 +0530

    Time: 2 ms (82.39%), Space: 46.1 MB (22.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,14 @@
 class Solution {
     public static void moveZeroes(int[] nums) {
-        int insertPos=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[insertPos]=nums[i];
-                insertPos++;
-            }
+      int insertPos = 0;
+      for(int i = 0;i<nums.length;i++){
+        if(nums[i]!=0){
+            nums[insertPos] = nums[i];
+            insertPos++;
         }
-        for(int i=insertPos;i<nums.length;i++){
-            nums[i]=0;
-        }
-    }
-    public static void main(String []args){
-        int nums[]={0,1,0,3,12};
-        moveZeroes(nums);
-        System.out.println("The array after moving zeroes is: ");
-        for(int i=0;i<nums.length;i++){
-            System.out.print(nums[i]+" ");
-        }
+      }
+      for(int i = insertPos; i<nums.length ;i++){
+           nums[i] = 0;
+      }
     }
 }
