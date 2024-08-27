ea030233719fbef28d331b212be571ff47ed9612
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-27 16:52:51 +0530

    Time: 0 ms (100%), Space: 57.4 MB (50.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -8,7 +8,7 @@ reverse(nums,0,n-1);
 
 }
 public static void reverse(int arr[],int start, int end){
-    while(start <= end){
+    while(start < end){
        int temp = arr[start];
        arr[start] = arr[end];
        arr[end] = temp;
