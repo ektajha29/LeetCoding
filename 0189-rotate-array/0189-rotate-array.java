4189dbcc04b5bc77be288c2dee8b6ec4c99826fd
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-27 16:52:08 +0530

    Time: 0 ms (100%), Space: 57.3 MB (63.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,19 @@
 class Solution {
     public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        if (n == 0 || k <= 0) return; 
-        k = k % n; 
-        if (k == 0) return; 
-        int[] temp = new int[n];
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-        System.arraycopy(temp, 0, nums, 0, n);
-    }
+int n = nums.length;
+ k = k % n;
+reverse(nums,0,n-k-1);
+reverse(nums,n-k,n-1);
+reverse(nums,0,n-1);
 
-    public static void main(String[] args) {
-        int k = 2;
-        int arr[] = {1, 2, 3, 4, 5};
-        rotate(arr, k); 
-        System.out.println("The array after right rotation by " + k + " elements is: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
 }
+public static void reverse(int arr[],int start, int end){
+    while(start <= end){
+       int temp = arr[start];
+       arr[start] = arr[end];
+       arr[end] = temp;
+       start++;
+       end--;
+           }
+}
+    }
