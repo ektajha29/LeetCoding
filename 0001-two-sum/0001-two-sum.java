f39be1e136c7f36f6967889591fd3a1064528776
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-08-28 00:39:17 +0530

    Time: 38 ms (47.4%), Space: 45.2 MB (10.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,13 +15,4 @@ class Solution {
         return ans;
     }
 
-    public static void main(String args[]) {
-        int n = 5;
-        int[] arr = {2, 6, 5, 8, 11};
-        int target = 14;
-        int[] ans = twoSum(arr, target);
-        System.out.println("This is the answer for variant 2: [" + ans[0] + ", "
-                           + ans[1] + "]");
-    }
-
 }
