ea6320b0d9f1c2041f2ca0b0036b7c4c72e8d697
Ekta Jha <144406877+ektajha29@users.noreply.github.com>
2024-09-13 02:06:14 +0530

    Time: 1 ms (99.79%), Space: 52.8 MB (60.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,33 +1,27 @@
 import java.util.*;
 class Solution {
     public static int majorityElement(int[] nums) {
-   
-        int n = nums.length;
-        int cnt = 0; 
-        int el = 0; 
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
-            } else if (el == nums[i]) cnt++;
-            else cnt--;
-        }
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) cnt1++;
-        }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1;
+   int n = nums.length;
+   int cnt =0;
+   int ele =0;
+   for(int i =0;i<n;i++){
+    if(cnt == 0){
+    cnt = 1;
+    ele = nums[i];
     }
-
-    public static void main(String args[]) {
-        int[] arr = {2, 2, 1, 1, 1, 2, 2};
-        int ans = majorityElement(arr);
-        System.out.println("The majority element is: " + ans);
-
-    }
-
+   else if(ele == nums[i])
+    cnt++;
+   else
+    cnt--;
 }
-
+int cnt1=0;
+for(int i=0;i<n;i++){
+if(nums[i] == ele)
+    cnt1++;
+}
+if(cnt1 > (n/2))
+return ele;
+return -1;
+    }
+}
  
